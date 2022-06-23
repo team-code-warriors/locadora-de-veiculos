43f7ec962ce85566854532a69fc55728435bd4fe
Ane Luisy <algrizotti@gmail.com>
2022-06-23 15:40:37 -0300

    Implementado Menu Principal e Módulo Taxa

---
+++ LocadoraDeVeiculos.Dominio.Tests/ModuloTaxa/ValidadorTaxaTest.cs
@@ -0,0 +1,107 @@
+﻿using FluentValidation.TestHelper;
+using LocadoraDeVeiculos.Dominio.ModuloTaxa;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LocadoraDeVeiculos.Dominio.Tests.ModuloTaxa
+{
+    [TestClass]
+    public class ValidadorTaxaTest 
+    {
+        private readonly Taxa taxa;
+        private readonly ValidadorTaxa validador;
+        public ValidadorTaxaTest()
+        {
+            taxa = new()
+            {
+                Descricao = "Cadeira de bebê",
+                Valor = 45.5,
+                TipoCalculo = "Fixo"
+            };
+
+            validador = new ValidadorTaxa();
+        }
+        [TestMethod]
+        public void Descricao_Nao_Pode_Ser_Nulo()
+        {
+            // arrange
+            taxa.Descricao = null;
+
+            // action
+            var resultado = validador.TestValidate(taxa);
+
+            // assert
+            resultado.ShouldHaveValidationErrorFor(f => f.Descricao);
+        }
+
+        [TestMethod]
+        public void Descricao_Nao_Pode_Ser_Vazio()
+        {
+            // arrange
+            taxa.Descricao = "";
+
+            // action
+            var resultado = validador.TestValidate(taxa);
+
+            // assert
+            resultado.ShouldHaveValidationErrorFor(f => f.Descricao);
+        }
+
+        [TestMethod]
+        public void Descricao_Deve_Ter_No_Minimo_2_Caracteres()
+        {
+            // arrange
+            taxa.Descricao = "a";
+
+            // action
+            var resultado = validador.TestValidate(taxa);
+
+            // assert
+            resultado.ShouldHaveValidationErrorFor(f => f.Descricao);
+        }
+
+        //[TestMethod]
+        //public void Valor_Aceita_Apenas_Numeros()
+        //{
+
+        //}
+
+        [TestMethod]
+        public void Valor_Deve_Ser_Maior_Que_0()
+        {
+            // arrange
+            taxa.Valor = 0;
+
+            // action
+            var resultado = validador.TestValidate(taxa);
+
+            // assert
+            resultado.ShouldHaveValidationErrorFor(f => f.Valor);
+        }
+
+        [TestMethod]
+        public void TipoCalculo_Nao_Pode_Ser_Nulo()
+        {
+            // arrange
+            taxa.TipoCalculo = null;
+
+            // action
+            var resultado = validador.TestValidate(taxa);
+
+            // assert
+            resultado.ShouldHaveValidationErrorFor(f => f.TipoCalculo);
+        }
+
+        [TestMethod]
+        public void TipoCalculo_Nao_Pode_Ser_Vazio()
+        {
+            // arrange
+            taxa.TipoCalculo = "";
+
+            // action
+            var resultado = validador.TestValidate(taxa);
+
+            // assert
+            resultado.ShouldHaveValidationErrorFor(f => f.TipoCalculo);
+        }
+    }
+}
--- LocadoraDeVeiculos.WinFormsApp/ModuloTaxa/TabelaTaxaControl.Designer.cs
+++ LocadoraDeVeiculos.WinFormsApp/ModuloTaxa/TabelaTaxaControl.Designer.cs
@@ -28,10 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grid.Name = "grid";
+            this.grid.RowHeadersWidth = 51;
+            this.grid.RowTemplate.Height = 25;
+            this.grid.Size = new System.Drawing.Size(524, 383);
+            this.grid.TabIndex = 1;
+            // 
+            // TabelaTaxaControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grid);
+            this.Name = "TabelaTaxaControl";
+            this.Size = new System.Drawing.Size(524, 383);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataGridView grid;
     }
 }
