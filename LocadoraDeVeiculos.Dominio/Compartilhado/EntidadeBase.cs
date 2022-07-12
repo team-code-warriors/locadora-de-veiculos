8825d8d56c838d45fccbc6656385a194ddd195a9
anegrizotti <algrizotti@gmail.com>
2022-07-12 13:54:42 -0300

    Entidade Base refatorada com GUID

--- LocadoraDeVeiculos.Dominio/Compartilhado/EntidadeBase.cs
+++ LocadoraDeVeiculos.Dominio/Compartilhado/EntidadeBase.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Taikandi;
 
 namespace LocadoraDeVeiculos.Dominio.Compartilhado
 {
     public abstract class EntidadeBase<T>
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+
+        public EntidadeBase()
+        {
+            Id = SequentialGuid.NewGuid();
+        }
 
     }
 }
