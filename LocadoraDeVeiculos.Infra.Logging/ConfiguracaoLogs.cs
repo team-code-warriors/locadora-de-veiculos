2759fa1696ac83bed4c13dc62d7c563b15eee1a6
anegrizotti <algrizotti@gmail.com>
2022-07-14 14:28:46 -0300

    Escritas de Logs configurados para WEB

--- LocadoraDeVeiculos.Infra.Logging/ConfiguracaoLogs.cs
+++ LocadoraDeVeiculos.Infra.Logging/ConfiguracaoLogs.cs
@@ -22,12 +22,18 @@ namespace LocadoraDeVeiculos.Infra.Logging
                 .GetSection("DiretorioSaida")
                 .Value;
 
-            Log.Logger = new LoggerConfiguration()
+            var configuracaoLogEmArquivo = new LoggerConfiguration()
                    .MinimumLevel.Debug()
                    .WriteTo.File(diretorioSaida + "/log.txt",
                rollingInterval: RollingInterval.Day,
                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
                .CreateLogger();
+
+            var configuracaoLogNaWeb = new LoggerConfiguration()
+                   .MinimumLevel.Debug()
+                   .WriteTo.Seq("http://localhost:5341");
+
+            Log.Logger = configuracaoLogNaWeb.CreateLogger();
         }
     }
 }
