2f90233031d04fdd386ba33b3ed114cfb45e9390
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-07-14 17:33:15 -0300

    errros nos parametros dos controladores

--- LocadoraDeVeiculos.WinFormsApp/Compartilhado/TelaMenuPrincipal.cs
+++ LocadoraDeVeiculos.WinFormsApp/Compartilhado/TelaMenuPrincipal.cs
@@ -176,10 +176,10 @@ namespace LocadoraDeVeiculos.WinFormsApp
 
             controladores = new Dictionary<string, ControladorBase>();
 
-            controladores.Add("Funcionários", new ControladorFuncionario(repositorioFuncionario, servicoFuncionario));
+            controladores.Add("Funcionários", new ControladorFuncionario(servicoFuncionario));
             controladores.Add("Clientes", new ControladorCliente(servicoCliente));
             controladores.Add("Funcionários", new ControladorFuncionario(servicoFuncionario));
-            controladores.Add("Clientes", new ControladorCliente(repositorioCliente, servicoCliente));
+            controladores.Add("Clientes", new ControladorCliente(servicoCliente));
             controladores.Add("Condutores", new ControladorCondutor(repositorioCondutor, servicoCondutor));
             controladores.Add("Taxas", new ControladorTaxa(servicoTaxa));
             controladores.Add("Grupo de Veículos", new ControladorGrupoDeVeiculos(repositorioGrupoDeVeiculos, servicoGrupoVeiculo));
