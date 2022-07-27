4c4fed8963a230f754f6b7d7453c3fee7a0770c5
anegrizotti <algrizotti@gmail.com>
2022-07-27 15:57:43 -0300

    Metodo Devolver implementado

--- LocadoraDeVeiculos.Aplicacao/ModuloLocacao/ServicoLocacao.cs
+++ LocadoraDeVeiculos.Aplicacao/ModuloLocacao/ServicoLocacao.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentResults;
+using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,9 @@ namespace LocadoraDeVeiculos.Aplicacao.ModuloLocacao
 {
     public class ServicoLocacao
     {
+        public Result<Locacao> SelecionarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- LocadoraDeVeiculos.WinFormsApp/ModuloLocacao/ControladorLocacao.cs
+++ LocadoraDeVeiculos.WinFormsApp/ModuloLocacao/ControladorLocacao.cs
@@ -1,4 +1,6 @@
-﻿using LocadoraDeVeiculos.WinFormsApp.Compartilhado;
+﻿using LocadoraDeVeiculos.Aplicacao.ModuloLocacao;
+using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using LocadoraDeVeiculos.WinFormsApp.Compartilhado;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,9 @@ namespace LocadoraDeVeiculos.WinFormsApp.ModuloLocacao
 {
     public class ControladorLocacao : ControladorBase
     {
+        private TabelaLocacaoControl tabelaLocacoes;
+        private readonly ServicoLocacao servicoLocacao;
+
         public override void Editar()
         {
             throw new NotImplementedException();
@@ -26,7 +31,27 @@ namespace LocadoraDeVeiculos.WinFormsApp.ModuloLocacao
 
         public override void Devolver()
         {
-            throw new NotImplementedException();
+            var id = tabelaLocacoes.ObtemIdCondutorSelecionada();
+
+            if (id == Guid.Empty)
+            {
+                MessageBox.Show("Selecione uma locação primeiro",
+                    "Cadastro de Devolução", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var resultado = servicoLocacao.SelecionarPorId(id);
+
+            if (resultado.IsFailed)
+            {
+                MessageBox.Show(resultado.Errors[0].Message,
+                    "Edição de Condutores", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var locacaoSelecionada = resultado.Value;
+
+            locacaoSelecionada.Status = StatusLocacaoEnum.Inativa;
         }
 
         public override ConfiguracaoToolboxBase ObtemConfiguracaoToolbox()
