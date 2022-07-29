e25920f36b14977a05bc47ed5363b2cca5990771
anegrizotti <algrizotti@gmail.com>
2022-07-29 17:30:10 -0300

    base dos testes de integração criada

--- LocadoraDeVeiculos.Dominio.Tests/ModuloLocacao/ValidadorLocacaoTest.cs
+++ LocadoraDeVeiculos.Dominio.Tests/ModuloLocacao/ValidadorLocacaoTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Dominio.Tests.ModuloLocacao
 {
-    internal class ValidadorLocacaoTest
+    [TestClass]
+    public class ValidadorLocacaoTest
     {
+        private Locacao locacao;
+        private readonly ValidadorLocacao validador;
+
+        public ValidadorLocacaoTest()
+        {
+            validador = new();
+        }
     }
 }
--- LocadoraDeVeiculos.Infra.BancoDeDados.Tests/ModuloLocacao/RepositorioLocacaoOrmTest.cs
+++ LocadoraDeVeiculos.Infra.BancoDeDados.Tests/ModuloLocacao/RepositorioLocacaoOrmTest.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿using FluentAssertions;
+using LocadoraDeVeiculos.Dominio.ModuloCliente;
+using LocadoraDeVeiculos.Dominio.ModuloCondutor;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoDeVeiculos;
+using LocadoraDeVeiculos.Dominio.ModuloLocacao;
+using LocadoraDeVeiculos.Dominio.ModuloPlanoDeCobranca;
+using LocadoraDeVeiculos.Dominio.ModuloTaxa;
+using LocadoraDeVeiculos.Dominio.ModuloVeiculo;
+using LocadoraDeVeiculos.Infra.BancoDeDados.Tests.Compartilhado;
+using LocadoraDeVeiculos.Infra.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infra.Orm.ModuloCliente;
+using LocadoraDeVeiculos.Infra.Orm.ModuloCondutor;
+using LocadoraDeVeiculos.Infra.Orm.ModuloFuncionario;
+using LocadoraDeVeiculos.Infra.Orm.ModuloLocacao;
+using LocadoraDeVeiculos.Infra.Orm.ModuloPlanoDeCobranca;
+using LocadoraDeVeiculos.Infra.Orm.ModuloTaxa;
+using LocadoraDeVeiculos.Infra.Orm.ModuloVeiculo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +25,90 @@ using System.Threading.Tasks;
 
 namespace LocadoraDeVeiculos.Infra.BancoDeDados.Tests.ModuloLocacao
 {
-    internal class RepositorioLocacaoOrmTest
+    [TestClass]
+    public class RepositorioLocacaoOrmTest : IntegrationTestBase
     {
+        private RepositorioFuncionarioOrm repositorioFuncionario;
+        private RepositorioCondutorOrm repositorioCondutor;
+        private RepositorioVeiculoOrm repositorioVeiculo;
+        private RepositorioPlanoDeCobrancaOrm repositorioPlano;
+        private RepositorioTaxaOrm repositorioTaxa;
+        private RepositorioClienteOrm repositorioCliente;
+        private RepositorioLocacaoOrm repositorio;
+        private readonly LocadoraDeVeiculosDbContext dbContext;
+        byte[] byteItems = new byte[] { 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10 };
+
+        public RepositorioLocacaoOrmTest()
+        {
+            this.dbContext = dbContext;
+            repositorioFuncionario = new RepositorioFuncionarioOrm(dbContext);
+            repositorioCondutor = new RepositorioCondutorOrm(dbContext);
+            repositorioVeiculo = new RepositorioVeiculoOrm(dbContext);
+            repositorioPlano = new RepositorioPlanoDeCobrancaOrm(dbContext);
+            repositorioTaxa = new RepositorioTaxaOrm(dbContext);
+            repositorioCliente = new RepositorioClienteOrm(dbContext);
+            repositorio = new RepositorioLocacaoOrm(dbContext);
+        }
+
+        #region criação de instancias
+        private Cliente NovoCliente()
+        {
+            Cliente c = new Cliente("Lucas Bleyer", "lucas@gmail.com", "Lages", "111.222.333-44", "43.367.658/0001-49", "(11) 99999-9999");
+            repositorioCliente.Inserir(c);
+
+            return c;
+        }
+
+        private Condutor NovoCondutor()
+        {
+            return new Condutor(NovoCliente(), "Lucas Bleyer", "111.222.333-44", "12345678901", DateTime.Now.Date, "lucas@gmail.com", "(11) 99999-9999", "Lages");
+        }
+
+        private GrupoDeVeiculos NovoGrupo()
+        {
+            return new GrupoDeVeiculos("SUV");
+        }
+
+        private PlanoDeCobranca NovoPlano()
+        {
+            return new PlanoDeCobranca(NovoGrupo(), "Plano Diário", 100, 10, 10);
+        }
+
+        private Funcionario NovoFuncionario()
+        {
+            return new Funcionario("Fabricio Silva", 1000, DateTime.Today.Date, "fabsilv11", "123453fab", "Comum");
+        }
+
+        private Taxa NovaTaxa()
+        {
+            return new Taxa("GPS", 10, "Fixo");
+        }
+
+        private Veiculo NovoVeiculo()
+        {
+            return new Veiculo("Focus", "Ford", 2000, "Manual", "Branco", "AAAA124", 100, "Gasolina", 1000, NovoGrupo(), byteItems);
+        }
+
+        private Locacao NovaLocacao()
+        {
+            return new Locacao(NovoFuncionario(), NovoCondutor(), NovoVeiculo(), NovoPlano(), new List<Taxa>(), DateTime.Today.Date, DateTime.Today.Date.AddDays(1), 100, StatusLocacaoEnum.Ativa);
+        }
+        #endregion
+
+        [TestMethod]
+        public void Deve_inserir_uma_locacao()
+        {
+            //arrange
+            var locacao = NovaLocacao();
+
+            //action
+            repositorio.Inserir(locacao);
+
+            //assert
+            var locacaoEncontrada = repositorio.SelecionarPorId(locacao.Id);
+
+            locacaoEncontrada.Should().NotBeNull();
+            locacaoEncontrada.Should().Be(locacao);
+        }
     }
 }
